350e5f109f0ce8a8eedb4f920b28b09389d4d1f9
Madeline Wong <madelinegwong@outlook.com>
2018-03-08 19:27:38 -0500

    created mock inital state for products.

--- src/products/index.js
+++ src/products/index.js
@@ -1,6 +1,25 @@
 
-
-const reducer = (state={}, action) => {
+const initalState = [
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Javelin (caution)",
+        price: 500.00,
+        count: 0
+    },
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Albino Snail", 
+        price: 3.99,
+        count: 0
+    },
+    {
+        imgSrc: "https://via.placeholder.com/100x100",
+        name: "Rug",
+        price: 400.00,
+        count: 0
+    }
+];
+const reducer = (state=initalState, action) => {
     console.log('product stuff');
     return state;
 }
