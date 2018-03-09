3406ad0ecb3696b0dd80d0d48476de6a0676dc9f
Madeline Wong <madelinegwong@outlook.com>
2018-03-08 21:01:03 -0500

    added functionality to the increment/decrements.

--- src/products/index.js
+++ src/products/index.js
@@ -1,45 +1,49 @@
-
 const initalState = [
-    {
-        imgSrc: "https://via.placeholder.com/100x100",
-        name: "Javelin (caution)",
-        price: 500.00,
-        count: 0
-    },
-    {
-        imgSrc: "https://via.placeholder.com/100x100",
-        name: "Albino Snail", 
-        price: 3.99,
-        count: 0
-    },
-    {
-        imgSrc: "https://via.placeholder.com/100x100",
-        name: "Rug",
-        price: 400.00,
-        count: 0
-    }
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Javelin (caution)",
+    price: 500.0,
+    count: 0
+  },
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Albino Snail",
+    price: 3.99,
+    count: 0
+  },
+  {
+    imgSrc: "https://via.placeholder.com/100x100",
+    name: "Rug",
+    price: 400.0,
+    count: 0
+  }
 ];
-const reducer = (state=initalState, action) => {
-    switch(action.type){
-        case "ADD_ITEM":
-            const position = action.payload.value;
-            const selectedItem = state[position];
-            const before = state.slice(0, position);
-            const after = state.slice (position + 1);
-            const newProduct = {
-                ...selectedItem,
-                count: selectedItem +1 
-                };
-            return [
-                    ...before,
-                    newProduct,
-                    ...after
-            ];
-        case "REMOVE_ITEM":
-            return state;
-        default:
-            return state;
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      const position = action.payload.value;
+      const selectedItem = state[position];
+      const before = state.slice(0, position);
+      const after = state.slice(position + 1);
+      const newProduct = {
+        ...selectedItem,
+        count: selectedItem.count + 1
+      };
+      return [...before, newProduct, ...after]; }
+    case "REMOVE_ITEM": {
+      const position = action.payload.value;
+      const selectedItem = state[position];
+      const before = state.slice(0, position);
+      const after = state.slice(position + 1);
+      const newProduct = {
+        ...selectedItem,
+        count: selectedItem.count === 0 ? 0 : selectedItem.count - 1
+      };
+      return [...before, newProduct, ...after];
     }
-}
+    default: 
+    return state;
+  }
+};
 
 export default reducer;
