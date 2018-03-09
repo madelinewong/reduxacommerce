02fa16c6ee784578d9c9f87ad4f9e5732ba1bfb8
Madeline Wong <madelinegwong@outlook.com>
2018-03-08 20:32:29 -0500

    connected products and added functionality to add and remove.

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 const Product = props => {
     return (
       <li>
         <img src={props.imgSrc} alt={props.name} />
         <p>{props.name}</p>
         <p>${props.price}</p>
-        <button onClick={() => console.log("+")}>+</button>
+        <button onClick={() => props.dispatch({type: 'ADD_ITEM', payload: { value: props.position}})}>+</button>
         <p>{props.count}</p>
-        <button onClick={() => console.log("-")}>-</button>
+        <button onClick={() => props.dispatch({type: 'REMOVE_ITEM', payload: { value: props.position}})}>-</button>
       </li>
     );
   };
+  
+  const mapStateToProps = null;
+  const mapDispatchToProps = null;
 
-  export default Product;
+  export default connect(mapStateToProps, mapDispatchToProps)(Product);
