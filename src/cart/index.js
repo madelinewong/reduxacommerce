0345b822ef47e72db8b74121c1e51eb3a985289d
Madeline Wong <madelinegwong@outlook.com>
2018-03-12 20:09:12 -0400

    used redux types, action, and reducer for the cart

--- src/cart/index.js
+++ src/cart/index.js
@@ -1,8 +1,18 @@
+import * as types from './types';
+import * as cartActions from './actions';
 
+const initialState = false;
 
-const reducer = (state={}, action) => {
-    console.log('cart stuff');
-    return state;
+const reducer = (state=initialState, action) => {
+    switch(action.type){
+        case types.TOGGLE_CART: {
+            return state === false ? true : false;
+        }
+        default: return state;
+    }
+}
+export {
+    cartActions
 }
 
 export default reducer;
