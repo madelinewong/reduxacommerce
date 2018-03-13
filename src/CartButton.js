9d3a1697cd1743293275693d542ed981d4df2372
Madeline Wong <madelinegwong@outlook.com>
2018-03-12 20:41:27 -0400

    calculated the cart products count in button

--- src/CartButton.js
+++ src/CartButton.js
@@ -3,14 +3,23 @@ import { connect } from 'react-redux';
 import { cartActions } from './cart';
 
 const CartButton = props => {
+    const count = Object.keys(props.products)
+                                   .map(key => props.products[key])
+                                   .map(p => p.count)
+                                   .reduce((prev, next)=> prev + next, 0);
     return (
         <div>
+            <p>{count}</p>
         <button onClick={() => props.toggle()}>Cart</button>
         </div>
     )
 }
 
-const mapStateToProps = null;
+const mapStateToProps = state => {
+    return {
+        products: state.products
+    }
+};
 
 const mapDispatchToProps = dispatch => {
     return {
